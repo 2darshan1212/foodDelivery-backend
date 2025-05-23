fe8843fa5ce6bc3389817a9f8ca0a2b301b66eaa
2darshan1212 <darshamisaal1212@gmail.com>
2025-05-23 18:46:56 +0530

    updated backend with lid

--- index.js
+++ index.js
@@ -37,12 +37,23 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
 // Make io available throughout the app
 app.set("io", io);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins[0]);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 
 app.use(express.json());
 app.use(cookieParser());
